a6fa7feefe1f438c2878724780c2d3597af41cea
Rai-Carvalho <raiarturc@gmail.com>
2024-09-13 14:26:06 -0300

    apresenta metas realizadas

--- index.js
+++ index.js
@@ -46,7 +46,23 @@ const listarMetas = async () => {
     })
 
     console.log("Meta(s) concluída(s)")
-}   
+}  
+
+const metasRealizadas = async () => {
+    const realizadas =  metas.filter((meta) => {
+        return meta.checked
+    })
+    
+    if(realizadas.length == 0) {
+        console.log('Não existem metas realizadas!')
+        return
+    }
+
+    await select({
+        message: "Metas Realizadas",
+        choices: [...realizadas]
+    })
+}
 
 const start = async () => {
     
@@ -64,6 +80,10 @@ const start = async () => {
                     value: "listar"
                 },
                 {
+                    name: "Metas realizadas",
+                    value: "realizadas"
+                },
+                {
                     name: "Sair",
                     value: "sair"
                 }
@@ -77,6 +97,9 @@ const start = async () => {
                 break
             case "listar":
                 await listarMetas()
+                break
+            case "realizadas":
+                await metasRealizadas()
                 break
             case "sair":
                 console.log("Até a próxima!")
